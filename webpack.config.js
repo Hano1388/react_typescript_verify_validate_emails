0203cd796dad44deaa886d5406f4daeb58474552
Hano <hinsul3113@gmail.com>
2018-03-19 00:34:18 -0700

    installed dependencies, configured webpack and typescript

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path              = require('path');
+module.exports = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: ['source-map-loader']
+      },
+    ]
+  }
+}
